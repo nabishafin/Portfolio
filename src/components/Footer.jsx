bcbd86af96593b7e7b5ce6c611fc2d775e5c25d4
nabishafin <shafin21215@gmail.com>
2025-05-03 16:23:26 +0600

    ok

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className="bg-gray-900 text-white pt-12 pb-8">
+        <footer className="bg-gradient-to-r from-black via-purple-950 to-black text-white pt-12 pb-8">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-1 md:grid-cols-4 gap-8 text-sm">
 
                 {/* About Section */}
